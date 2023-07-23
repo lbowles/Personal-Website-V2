6d98adb3bad67b86c310186a64c57b6d29d2fa7e
lbowles <lspbowles@gmail.com>
2023-07-23 10:24:27 +0200

    remove button in link and replace with div, routing wasnt working

--- portfolio/src/components/LinkButton.tsx
+++ portfolio/src/components/LinkButton.tsx
@@ -8,9 +8,9 @@ interface StyledLinkProps {
 const StyledLink: React.FC<StyledLinkProps> = ({ href, children }) => {
   return (
     <Link href={href}>
-      <button className="bg-black h-[20px] rounded-full block m-0 px-3  text-white text-sm font-normal hover:bg-gray-700 hover:pointer">
+      <div className="bg-black h-[20px] rounded-full block m-0 px-3  text-white text-sm font-normal hover:bg-gray-700 hover:pointer">
         {children}
-      </button>
+      </div>
     </Link>
   );
 };
